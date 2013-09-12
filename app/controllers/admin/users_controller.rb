eb28bc6680f1e903145f87e767678ba2f12d8525
Sergey Vatagin <s.vatagin@gmail.com>
2013-09-12 23:00:03 +0400

    add add_car_wash to users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < ApplicationController
   layout 'admin'
 
-  #before_filter :find_user, :only => [:edit, :update, :show, :destroy]
+  before_filter :find_user, :only => [:edit, :update, :show, :destroy ,:add_car_wash]
 
   def index
     #@users = User.all
@@ -14,4 +14,37 @@ class Admin::UsersController < ApplicationController
       format.json { render json: @users}
     end
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def add_car_wash
+  end
+
+  def update
+    if @user.update_attributes(user_params) && @user.make_client && @user.revoke_guest
+      redirect_to admin_users_path, :notice => "User successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path, :notice => "User deleted."
+  end
+
+  protected
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :salt, :encrypted_password, :car_wash_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Moika::Application.routes.draw do
   namespace :admin do
     get '', to: 'dashboard#main', as: '/'
     resources :users
+    get 'add_car_wash/:id', to: 'users#add_car_wash', as: '/add_car_wash'
+
   end
 end
