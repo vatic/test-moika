00a1347afd84c7a427f55e8404faf7bd8114d75f
Sergey Vatagin <s.vatagin@gmail.com>
2013-09-16 16:06:01 +0400

    change icons on map and add geocoder if

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,6 +1,6 @@
 class MapController < ApplicationController
   layout "main_page"
   def show
-    #car_washes = CarWash.all
+    @car_washes = CarWash.all
   end
 end
--- app/models/car_wash.rb
+++ app/models/car_wash.rb
@@ -1,5 +1,10 @@
 class CarWash < ActiveRecord::Base
   has_many :users
   geocoded_by :address, :latitude  => :lat, :longitude => :lon
-  after_validation :geocode
+  after_validation :geocode, if: "lat_and_lon_nil?"
+
+  private
+    def lat_and_lon_nil?
+      self.lat.nil? && self.lon.nil?
+    end
 end
