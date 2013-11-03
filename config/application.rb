9d7ca2297568e066b82bdc9c9e82f1f8ff708fcd
Sergey Vatagin <s.vatagin@gmail.com>
2013-11-03 17:30:26 +0400

    add locale :ru to application.rb in addition to default_locale :ru

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Moika
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.locale = :ru
     config.i18n.default_locale = :ru
     config.autoload_paths += Dir["#{config.root}/lib", "#{config.root}/lib/**/"]
     config.assets.precompile += %w( admin.css main_page.js admin.js)
