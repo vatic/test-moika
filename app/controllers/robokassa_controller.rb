b00dceaee5e60004d6026d4de0c1e0feaecbb722
Sergey Vatagin <s.vatagin@gmail.com>
2013-11-18 19:52:14 +0400

    add shp prefix to car_wash_id

--- app/controllers/robokassa_controller.rb
+++ app/controllers/robokassa_controller.rb
@@ -1,14 +1,14 @@
 class RobokassaController < Robokassa::Controller
   def success
     super
-    @car_wash = CarWash.find(params[:car_wash_id])
+    @car_wash = CarWash.find(params[:shpcar_wash_id])
     @payment = @car_wash.payments.find(params[:InvId])
     redirect_to edit_car_wash_path(@car_wash), notice: "Ваш платеж на сумму #{@payment.amount} руб. успешно принят. Спасибо!"
   end
 
   def fail
     super
-    @car_wash = CarWash.find(params[:car_wash_id])
+    @car_wash = CarWash.find(params[:shpcar_wash_id])
     @payment = @car_wash.payments.find(params[:InvId])
     redirect_to edit_car_wash_path(@car_wash), varning: "Во время принятия платежа возникла ошибка. Мы скоро разберемся!"
   end
