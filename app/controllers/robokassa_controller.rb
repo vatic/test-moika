f5ff88f8e2a38c60b5b0395263ec5b2e0a14306a
Sergey Vatagin <s.vatagin@gmail.com>
2013-11-18 19:27:46 +0400

    change redirect path in robokassa success and fail

--- app/controllers/robokassa_controller.rb
+++ app/controllers/robokassa_controller.rb
@@ -1,12 +1,15 @@
 class RobokassaController < Robokassa::Controller
   def success
     super
-    @payment = Payment.find(params[:InvId])
-    redirect_to dashboard_path, notice: "Ваш платеж на сумму #{@payment.amount} руб. успешно принят. Спасибо!"
+    @car_wash = CarWash.find(params[:car_wash_id])
+    @payment = @car_wash.payments.find(params[:InvId])
+    redirect_to edit_car_wash_path(@car_wash), notice: "Ваш платеж на сумму #{@payment.amount} руб. успешно принят. Спасибо!"
   end
 
   def fail
     super
-    redirect_to dashboard_path, varning: "Во время принятия платежа возникла ошибка. Мы скоро разберемся!"
+    @car_wash = CarWash.find(params[:car_wash_id])
+    @payment = @car_wash.payments.find(params[:InvId])
+    redirect_to edit_car_wash_path(@car_wash), varning: "Во время принятия платежа возникла ошибка. Мы скоро разберемся!"
   end
 end
