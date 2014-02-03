891d229ec406e4b8f0fc92282df850b5a3bbb644
Sergey Vatagin <s.vatagin@gmail.com>
2014-02-03 16:51:08 +0400

    add user type choice in login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,11 +14,11 @@ class ApplicationController < ActionController::Base
         edit_car_wash_path(resource.car_wash)
       elsif resource.admin?
         admin_path
-      elsif resource.normal_user?
-        user_path(resource)
       else
         super
       end
+    elsif resource.is_a?(NormalUser)
+      normal_user_path(resource)
     else
       super
     end
